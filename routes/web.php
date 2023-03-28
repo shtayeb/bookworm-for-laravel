586afdc15e5d2d261a32e205b06ec2573b6a2289
Shahryar Tayeb <shahryar.dev@gmail.com>
2023-03-28 12:28:19 +0430

    removed testing routes

--- routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use SHTayeb\Bookworm\Http\Controllers\TestController;
-use Illuminate\Support\Facades\Route;
-
-Route::resource('roles', TestController::class);
--- src/BookwormServiceProvider.php
+++ src/BookwormServiceProvider.php
@@ -3,7 +3,6 @@
 namespace SHTayeb\Bookworm;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Route;
 class BookwormServiceProvider extends ServiceProvider
 {
     /**
@@ -19,14 +18,9 @@ class BookwormServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Route::prefix('bookworm')
-            ->as('bookworm.')
-            ->group(function () {
-                $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-            });
 
         if ($this->app->runningInConsole()) {
-            // In addition to publishing assets, we also publish the config
+            //  publish the config
             $this->publishes([
                 __DIR__.'/../config/bookworm.php' => config_path('bookworm.php'),
             ], 'shtayeb-bookworm-config');
